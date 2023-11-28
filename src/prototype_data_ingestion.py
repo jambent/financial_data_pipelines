e6fc62ddf6f5b711e7802dca03a5d8b25b8dfae5
jwbennett <jwbennett@Laptop>
2023-11-28 20:07:58 +0000

    Adds Currency Pair column to ingestion prototype

--- src/prototype_data_ingestion.py
+++ src/prototype_data_ingestion.py
@@ -1,14 +1,36 @@
 import yfinance as yf
+import pandas as pd
 from datetime import datetime as dt
 
 target_time = dt.fromisoformat('2023-11-28T16:30:00Z')
 
 ticker_list = [
+    'EURUSD=X',
     'GBPUSD=X',
-    'GBPEUR=X'
+    'GBPEUR=X',
+    'JPY=X',
+    'GBP=X',
 ]
+
+df = pd.DataFrame(columns= ('Open','High','Low','Close','Volume','Dividends','Stock Splits','Currency Pair'))
+
 for ticker in ticker_list:
     ticker_data = yf.Ticker(ticker)
-    df = ticker_data.history(period='1d', interval='15m')
-    filtered_df = df.loc[[target_time]]
-    print(filtered_df.head(24))
+    ticker_df = ticker_data.history(period='1d', interval='15m')
+    filtered_ticker_df = ticker_df.loc[[target_time]]
+    
+    ticker_components = ticker.split('=')
+    currency_one = ''
+    currency_two = ''
+    if len(ticker_components[0]) == 3:
+        currency_one = 'USD'
+        currency_two = ticker_components[0]
+    else:
+        currency_one = ticker_components[0][:3]
+        currency_two = ticker_components[0][3:]
+    currency_pair = currency_one + '/' + currency_two
+    filtered_ticker_df['Currency Pair'] = currency_pair
+    
+    df= pd.concat([df,filtered_ticker_df])
+
+print(df.head())
